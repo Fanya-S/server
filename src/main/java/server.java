afcc9b64f2fca3000752f5c312e7b0c4bc7adbee
fanya_s <fanya_s@bk.ru>
2021-09-05 22:44:01 +0500

    zxc

--- src/main/java/server.java
+++ src/main/java/server.java
@@ -1,5 +1,22 @@
+
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 public class server {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Hello world!");
+        Frontend frontend = new Frontend();
+
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(frontend), "/mirror");
+
+        server.start();
+        System.out.println("Server started");
+        server.join();
+
     }
 }
